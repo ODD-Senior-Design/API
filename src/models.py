1c1fc2947e184f77907a5bb1683cb8014092a865
eliteabhi <abhirangarajan2003@gmail.com>
2025-02-19 03:12:40 -0600

    Some progresss

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from uuid import UUID
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String as id_type
 from sqlalchemy.orm import (
 
     DeclarativeBase,
@@ -19,28 +19,31 @@ class _Base( DeclarativeBase ):
 
 class PatientsModel( _Base ):
     __tablename__ = 'patients'
-    id: Mapped[ UUID ] = mapped_column( primary_key=True )
+    id: Mapped[ UUID ] = mapped_column(id_type, primary_key=True )
     first_name: Mapped[ str ] = mapped_column( nullable=False )
     last_name: Mapped[ str ] = mapped_column( nullable=False )
 
 class ImageSetsModel( _Base ):
     __tablename__ = 'image_sets'
-    id: Mapped[ UUID ] = mapped_column( primary_key=True )
+    id: Mapped[ UUID ] = mapped_column(id_type, primary_key=True )
     patient_id: Mapped[ UUID ] = mapped_column( ForeignKey( 'patients.id' ) )
     patient: Mapped[ PatientsModel ] = relationship( PatientsModel, backref=backref( 'image_sets', uselist=False ) )
 
 class ImagesModel( _Base ):
     __tablename__ = 'images'
-    id: Mapped[ UUID ] = mapped_column( primary_key=True )
+    id: Mapped[ UUID ] = mapped_column(id_type, primary_key=True )
     set_id: Mapped[ UUID ] = mapped_column( ForeignKey( 'image_sets.id' ) )
+    patient_id: Mapped[ UUID ] = mapped_column( ForeignKey( 'patients.id' ) )
     set: Mapped[ ImageSetsModel ] = relationship( ImageSetsModel, backref=backref( 'images', uselist=False ) )
+    image_timestamp: Mapped[ datetime ] = mapped_column( nullable=False )
     uri: Mapped[ str ] = mapped_column( nullable=False, unique=True )
 
 class AssessmentsModel( _Base ):
     __tablename__ = 'assessments'
-    id: Mapped[ UUID ] = mapped_column( primary_key=True )
+    id: Mapped[ UUID ] = mapped_column(id_type, primary_key=True )
     image_id: Mapped[ UUID ] = mapped_column( ForeignKey( 'images.id' ) )
     set_id: Mapped[ UUID ] = mapped_column( ForeignKey( 'image_sets.id' ) )
+    patient_id: Mapped[ UUID ] = mapped_column( ForeignKey( 'patient.id' ) )
     image: Mapped[ ImagesModel ] = relationship( ImagesModel, backref=backref( 'assessments', uselist=False ) )
     assessment_timestamp: Mapped[ datetime ] = mapped_column( nullable=False )
     assessment: Mapped[ bool ] = mapped_column( nullable=False )
--- test/generate_sample_records.py
+++ test/generate_sample_records.py
@@ -1,5 +1,13 @@
+from datetime import datetime
+from os import getenv
+from dotenv import load_dotenv
 from jsf import JSF
 import json
+import sqlalchemy as sa
+from sqlalchemy.orm import Session
+import sys
+sys.path.insert(0, r'./src/')
+from models import PatientsModel, ImagesModel, ImageSetsModel, AssessmentsModel
 
 SAMPLE_SCHEMA_JSON_PATH = './test/sample_schema.json'
 SAMPLE_DATA_JSON_SAVE_PATH = './test/sample_data.json'
@@ -15,21 +23,65 @@ print( fake_data )
 # Replace sub-oobject ids with super objects id
 
 # Replace image_sets.patient.id with image_sets.patient_id
-for set in fake_data["image_sets"]:
-    set["patient"]["id"] = set["patient_id"]
+for set in fake_data[ 'image_sets' ]:
+    set[ 'patient' ][ 'id' ] = set[ 'patient_id' ]
 
-for image in fake_data["images"]:
-    image["image_set"]["id"] = image["set_id"]
-    image["image_set"]["patient"]["id"] = image["image_set"]["patient_id"]
+for image in fake_data[ 'images' ]:
+    image[ 'image_set' ][ 'id' ] = image[ 'set_id' ]
+    image[ 'image_set' ][ 'patient_id' ] = image[ 'patient_id' ]
+    image[ 'image_set' ][ 'patient' ][ 'id' ] = image[ 'image_set' ][ 'patient_id' ]
 
-for assesment in fake_data["assessments"]:
-    assesment["image"]["id"] = assesment["image_id"]
-    assesment["image"]["set_id"] = assesment["set_id"]
-    assesment["image"]["image_set"]["id"] = assesment["set_id"]
-    assesment["image"]["image_set"]["patient"]["id"] = assesment["image"]["image_set"]["patient_id"]
-
+for assessment in fake_data[ 'assessments' ]:
+    assessment[ 'image' ][ 'id' ] = assessment[ 'image_id' ]
+    assessment[ 'image' ][ 'set_id' ] = assessment[ 'set_id' ]
+    assessment[ 'image' ][ 'set_id' ] = assessment[ 'patient_id' ]
+    assessment[ 'image' ][ 'image_set' ][ 'id' ] = assessment[ 'set_id' ]
+    assessment[ 'image' ][ 'image_set' ][ 'patient_id' ] = assessment[ 'patient_id' ]
+    assessment[ 'image' ][ 'image_set' ][ 'patient' ][ 'id' ] = assessment[ 'patient_id' ]
 
 print('\n')
 
 with open( SAMPLE_DATA_JSON_SAVE_PATH, 'w', encoding='utf-8' ) as f:
     json.dump( fake_data, f, ensure_ascii=False, indent=4 )
+    
+print( getenv( 'DB_URI' ) )
+engine = sa.create_engine( getenv( 'DB_URI' ) or '' )
+
+def de_onion_dict( collection, popval ):
+    outer_values = [ dict(iset) for iset in collection ]
+    inner_values = list( map( lambda x: dict(x.pop( popval )), outer_values ) )
+    return outer_values, inner_values
+
+insert_patients = [ PatientsModel( **p ) for p in fake_data[ 'patients' ] ]
+
+image_set_values, image_set_patients = de_onion_dict( fake_data[ 'image_sets' ], 'patient' )
+
+insert_image_sets = [ ImageSetsModel( **iset ) for iset in image_set_values ]
+insert_patients.extend( PatientsModel( **p ) for p in image_set_patients )
+
+images_values, images_image_sets = de_onion_dict( fake_data[ 'images' ], 'image_set' )
+images_image_sets, images_image_set_patients = de_onion_dict( images_image_sets, 'patient' )
+
+#TODO: FIX DATETIME FOR IMAGES AND ASSESSMENTS
+
+insert_images = [ ImagesModel( **img ) for img in images_values ]
+insert_image_sets.extend( ImageSetsModel( **iset ) for iset in images_image_sets )
+insert_patients.extend( PatientsModel( **p ) for p in images_image_set_patients )
+
+assessment_values, assessment_images = de_onion_dict( fake_data[ 'assessments' ], 'image' )
+assessment_images, assessment_images_image_sets = de_onion_dict( assessment_images, 'image_set' )
+assessment_images_image_sets, assessment_images_image_sets_patients = de_onion_dict( assessment_images_image_sets, 'patient' )
+insert_assessments = [ AssessmentsModel( **a ) for a in assessment_values ]
+insert_images.extend( ImagesModel( **img ) for img in assessment_images )
+insert_image_sets.extend( ImageSetsModel( **iset ) for iset in assessment_images_image_sets )
+insert_patients.extend( PatientsModel( **p ) for p in assessment_images_image_sets_patients )
+
+with Session(engine) as conn:
+    conn.add_all( insert_assessments )
+    conn.add_all( insert_images )
+    conn.add_all( insert_image_sets )
+    conn.add_all( insert_patients )
+    conn.commit()
+
+if __name__ == '__main__':
+    load_dotenv()
