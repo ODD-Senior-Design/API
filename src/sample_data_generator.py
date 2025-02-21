a483bb0dfcebbdb9193904a638d45102b8a3f884
Abhi Rangarajan <abhirangarajan2003@gmail.com>
2025-02-20 18:05:29 -0600

    Split sample data schema up and re-implimented generation/consistency correction

--- src/sample_data_generator.py
+++ src/sample_data_generator.py
@@ -1,6 +1,6 @@
 from jsf import JSF
 import json
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, List, Any
 
 class DataGenerator():
     def __init__( self, json_schema_path, save_as_json=False, json_save_path=None ):
@@ -9,35 +9,43 @@ class DataGenerator():
         self.__json_save_path = json_save_path
 
     def __fix_inconsistencies( self, data: dict ) -> Dict[ str, Any ]:
-        for set in data[ 'image_sets' ]:
-            set[ 'patient' ][ 'id' ] = set[ 'patient_id' ]
 
-        for image in data[ 'images' ]:
-            image[ 'image_set' ][ 'id' ] = image[ 'set_id' ]
-            image[ 'image_set' ][ 'patient_id' ] = image[ 'patient_id' ]
-            image[ 'image_set' ][ 'patient' ][ 'id' ] = image[ 'image_set' ][ 'patient_id' ]
+        if data.get( 'image_id' ):
+            data[ 'image' ].update( { 'id': data[ 'image_id' ], 'set_id': data[ 'set_id' ], 'patient_id': data[ 'patient_id' ] } )
+            data[ 'image' ][ 'image_set' ].update( { 'id': data[ 'set_id' ], 'patient_id': data[ 'patient_id' ] } )
+            data[ 'image' ][ 'image_set' ][ 'patient' ][ 'id' ] = data[ 'patient_id' ]
 
-        for assessment in data[ 'assessments' ]:
-            assessment[ 'image' ][ 'id' ] = assessment[ 'image_id' ]
-            assessment[ 'image' ][ 'set_id' ] = assessment[ 'set_id' ]
-            assessment[ 'image' ][ 'patient_id' ] = assessment[ 'patient_id' ]
-            assessment[ 'image' ][ 'image_set' ][ 'id' ] = assessment[ 'set_id' ]
-            assessment[ 'image' ][ 'image_set' ][ 'patient_id' ] = assessment[ 'patient_id' ]
-            assessment[ 'image' ][ 'image_set' ][ 'patient' ][ 'id' ] = assessment[ 'patient_id' ]
+        elif data.get( 'set_id' ):
+            data[ 'image_set' ].update( { 'id': data[ 'set_id' ], 'patient_id': data[ 'patient_id' ] } )
+            data[ 'image_set' ][ 'patient' ][ 'id' ] = data[ 'patient_id' ]
+
+        elif data.get( 'patient_id' ):
+            data[ 'patient' ][ 'id' ] = data[ 'patient_id' ]
+
+
 
         return data.copy()
 
-
-    def generate_data( self, num=1 ) -> Optional[ Dict[ str, Any ] ]:
+    def generate_data( self, num=1 ) -> Optional[ List[ Dict[ str, Any ] ] ]:
         try:
             jsf = JSF.from_json( self.__json_schema_path )
         except Exception as e:
             print( f'Error occurred while loading JSON schema: {e}' )
             return None
 
-        sample_data = jsf.generate( num )
-        self.__fix_inconsistencies( sample_data )
-        print(sample_data)
+        sample_data = jsf.generate( n=num )
+        if num == 1:
+            sample_data = [ sample_data ]
+
+        if sample_data[0].get( 'assessments' ):
+            for sample in sample_data:
+                sample[ 'assessments' ] = [ self.__fix_inconsistencies( a ) for a in sample[ 'assessments' ] ]
+                sample[ 'images' ] = [ self.__fix_inconsistencies( i ) for i in sample[ 'images' ] ]
+                sample[ 'image_sets' ] = [ self.__fix_inconsistencies( s ) for s in sample[ 'image_sets' ] ]
+                sample[ 'patients' ] = [ self.__fix_inconsistencies( p ) for p in sample[ 'patients' ] ]
+        else:
+            sample_data = [ self.__fix_inconsistencies( d ) for d in sample_data ]
+        print( sample_data )
 
         if self.__save_as_json:
             with open( self.__json_save_path or 'generated_data.json', 'w' ) as f:
